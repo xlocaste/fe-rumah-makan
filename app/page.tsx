824b3a4effd0e07ec446462dcfa43d9b7a407348
xlocaste <irfandi.di122@gmail.com>
2024-09-20 13:56:56 +0700

    membuat login dan page rumah makan

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,109 @@
-import Image from "next/image";
+'use client'
+import axios from "axios";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
-export default function Home() {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [massage, setMassage] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8000/api/login", {
+        email,
+        password,
+      });
+      Cookies.set("token", response.data.token);
+      const user = response.data.data;
+      const role = user.role;
+   
+      setMassage('Login successful');
+      if (role === 'admin') {
+        router.push("/rumah-makan"); // Supervisor masuk ke form penugasan
+      } else if (role === 'pemilikUsaha') {
+        router.push("/menu"); // Karyawan masuk ke pengumpulan penugasan
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setMassage(
+          "login Gagal: " +
+            (error.response?.data.massage || "Mungkin Ada Kesalahan")
+        );
+      } else {
+        setMassage("Username atau password salah");
+      }
+    }
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-lg">
+        <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
+          Selamat Datang
+        </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <form
+          action="#"
+          onSubmit={handleLogin}
+          className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          {massage && <p className="text-center">{massage}</p>}
+          <p className="text-center text-lg font-medium">Masukan Akun Anda</p>
+
+          <div>
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+
+            <div className="relative">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="text-black w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Masukan email"
+                required
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+
+            <div className="relative">
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="text-black w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Masukan password"
+                required
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            Login
+          </button>
+
+          <p className="text-center text-sm text-gray-500">
+            belum punya akun?
+            <a className="underline" href="register">
+              Daftar
+            </a>
+          </p>
+        </form>
+      </div>
     </div>
   );
-}
+};
+
+export default Login;
---
+++ app/rumah-makan/page.tsx
@@ -0,0 +1,147 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+
+interface  DataRumahMakan{
+  id: number;
+  nama: string;
+  alamat: string;
+  jam_buka: string;
+  jam_tutup: string;
+  created_at: string;
+  updated_at: string;
+}
+
+const RumahMakan = () => {
+  const [daftarRumahMakan, setDaftarRumahMakan] = useState<DataRumahMakan[]>([]);
+  const [userRole, setUserRole] = useState<string | null>(null);
+  const router = useRouter();
+  const token = Cookies.get("token");
+
+  useEffect(() => {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    axios
+      .get("http://localhost:8000/api/rumah-makan")
+      .then((response) => setDaftarRumahMakan(response.data.data))
+      .catch((error) => console.error("Error fetching Rumah Makan items:", error));
+  }, []);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+        try {
+          const token = Cookies.get("token");
+          if (token) {
+            axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+            const userResponse = await axios.get("http://localhost:8000/api/user");
+            setUserRole(userResponse.data.role);
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      };
+  
+      fetchUserData();
+
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    axios
+  }, []);
+
+  const logout = async () => {
+    try {
+      await axios.post('http://localhost:8000/api/logout', {}, {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      Cookies.remove("token");
+
+      router.push("/");
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+
+  const deletePenugasan = (id: number) => {
+    axios
+      .delete(`http://localhost:8000/api/rumah-makan/${id}`)
+      .then(() =>
+        setDaftarRumahMakan(daftarRumahMakan.filter((daftarRumahMakan) => daftarRumahMakan.id !== id))
+      )
+      .catch((error) => console.error("Error deleting rumah-makan:", error));
+    alert("hapus data berhasil");
+  };
+  return (
+      <div className="min-h-screen bg-gray-100 py-8 px-4">
+        <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+          <div className="p-6">
+            <h1 className="text-2xl font-semibold text-gray-800 mb-6">
+              Data Rumah Makan
+            </h1>
+            <div className="mb-6">
+              <Link href="rumah-makan/create">
+                <button className="mr-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+                  Tambah Rumah Makan
+                </button>
+              </Link>
+              <button
+                onClick={logout}
+                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+              >
+                Logout
+              </button>
+            </div>
+
+            {daftarRumahMakan.length > 0 ? (
+              <ul>
+                {daftarRumahMakan.map((ItemRumahMakan) => (
+                  <li
+                    key={ItemRumahMakan.id}
+                    className="bg-gray-50 p-4 mb-4 rounded-lg shadow-sm flex items-center justify-between"
+                  >
+                    <div className="flex flex-col">
+                      <p className="text-black">
+                        Nama Rumah Makan : {ItemRumahMakan.nama}
+                      </p>
+                      <p className="text-black">
+                        Alamat : {ItemRumahMakan.alamat}
+                      </p>
+                      <p className="text-black">
+                        Jam Buka : {ItemRumahMakan.jam_buka} Sampai {ItemRumahMakan.jam_tutup}
+                      </p>
+                    </div>
+                    <div className="flex space-x-2">
+                    {userRole == "superVisor" && (
+                      <Link href={`/rumah-makan/${ItemRumahMakan.id}/edit`}>
+                        <button className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600">
+                          Edit
+                        </button>
+                      </Link>
+                    )}
+                      <button
+                        onClick={() => deletePenugasan(ItemRumahMakan.id)}
+                        className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                      >
+                        Hapus dari Daftar
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="font-bold flex justify-center items-center">
+                Daftar Rumah Makan Kosong.
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default RumahMakan;
